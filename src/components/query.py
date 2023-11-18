0a022877986406eec551ca53ddf61ba887fa37db
sc0v0ne <sc0v1n001@gmail.com>
2023-11-18 16:18:09 -0300

    feat: remove variable

--- src/components/query.py
+++ src/components/query.py
@@ -7,6 +7,7 @@ def recommends(
                 top_n: int,
                 extra_cols: dict,
                 ) -> pd.DataFrame:
+
     rename = name.upper()
     exists_title = len(dataset[dataset['title'].str.contains(rename)])
     
@@ -20,13 +21,13 @@ def recommends(
         return response_markdown('This is not number')
 
     extra_cols = [x for x, y in extra_cols.items() if y]
-    
+
     movie = dataset[dataset['title'] == rename][['clusters_genre_type']]
     reset_movie = movie.reset_index()
     reset_movie = reset_movie.at[0, 'clusters_genre_type']
     k_id = int(reset_movie)
-    cols_review = ['title', 'gender_type'] + extra_cols
-    result = dataset[dataset['clusters_genre_type'] == k_id][cols_review][:int(top_n)]
+    cols_view = ['title', 'gender_type'] + extra_cols
+    result = dataset[dataset['clusters_genre_type'] == k_id][cols_view][:int(top_n)]
     result.set_index('title')
 
     return response_recommends(result)
