38c07e6e3ce6c2f21f40a97461042733c6bdf9cc
sc0v0ne <sc0v1n001@gmail.com>
2023-11-19 13:28:00 -0300

    feat: update init_train

--- pipe/clusters.py
+++ pipe/clusters.py
@@ -5,25 +5,28 @@ import pandas as pd
 from sklearn.cluster import KMeans
 
 
-def init_train(path_input, data_train, all_data):
+def init_train(data_train, all_data):
     
-    print('Initialize Train')
     print('-' * 80)
-    
-    path_train = os.path.join(path_input, 'data', 'processed',  data_train)
+    print('Initialize Train')
+
+    PATTERN_PATH = os.path.join('/pipe', 'data',)
+    path_train = os.path.join(PATTERN_PATH, 'processed',  data_train)
     X_train = np.array(pd.read_csv(path_train))
 
-    kmeans_model = KMeans(n_clusters=277, random_state=123456)
+    kmeans_model = KMeans(n_clusters=277, random_state=123456, n_init='auto')
     y_clusters = kmeans_model.fit_predict(X_train)
-    
+    print('-' * 80)
+    print()
     print('Prepare new Dataframe')
-    path_dataset = os.path.join(path_input, 'data', 'processed', all_data)
+    path_dataset = os.path.join(PATTERN_PATH, 'processed', all_data)
     dataset = pd.read_csv(path_dataset)
-    dataset['clusters_genre_type'] = y_clusters
-    
+    dataset['clusters_gender'] = y_clusters
+    print('-' * 80)
+    print()
     print('Save outputs')
-    OUTPUT= os.path.join(path_input, 'data', 'final')
-    
+    OUTPUT= os.path.join(PATTERN_PATH, 'final')
+
     if not os.path.exists(OUTPUT):
         os.mkdir(OUTPUT)
 
@@ -31,11 +34,12 @@ def init_train(path_input, data_train, all_data):
     dataset.to_csv(f'{path_dataset}.csv', index=False)
     print('Sucefully data final')
 
-    
-    OUTPUT_MODEL = os.path.join(path_input, 'data', 'models')
+    OUTPUT_MODEL = os.path.join(PATTERN_PATH, 'models')
 
     if not os.path.exists(OUTPUT_MODEL):
         os.mkdir(OUTPUT_MODEL)
 
     model_path = os.path.join(OUTPUT_MODEL, 'model_kmeans_20231119')
     joblib.dump(kmeans_model, f'{model_path}.pkl')
+    print('Sucefully Model')
+    print('-' * 80)
